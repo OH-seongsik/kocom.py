f3ecf7377189bcb0cc22e12032b094b2b121675f
OH-seongsik <106724468+OH-seongsik@users.noreply.github.com>
2024-06-21 15:19:33 +0900

    Update kocom.py

--- kocom.py
+++ kocom.py
@@ -637,14 +637,14 @@ def publish_discovery(dev, sub=''):
         if logtxt != "" and config.get('Log', 'show_mqtt_publish') == 'True':
             logging.info(logtxt)
     elif dev == 'light':
-	     sub1 = room_h_dic.get(sub)
+	    
              for num in range(1, int(config.get('User', 'light_count'))+1):
-		
+
             	topic = 'homeassistant/light/kocom_{}_light{}/config'.format(sub1,num)
             	payload = {
-              	  'name': 'Kocom {} Light{}'.format(sub1, num),
-              	  'cmd_t': 'kocom/{}/light/{}/command'.format(sub1, num),
-              	  'stat_t': 'kocom/{}/light/state.format(sub1)',
+              	 'name': 'Kocom {} Light{}'.format(sub, num),
+              	 'cmd_t': 'kocom/{}/light/{}/command'.format(sub, num),
+              	 'stat_t': 'kocom/{}/light/state.format(sub)',
                	 'stat_val_tpl': '{{ value_json.light_' + str(num) + ' }}',
                	 'pl_on': 'on',
                	 'pl_off': 'off',
